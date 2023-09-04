13d40cb4996cbb222be5ce46010728e118e85ea1
Sebastian Mesa <sebas.mesa.montoya@gmail.com>
2023-09-03 20:08:56 -0500

    initial commit

---
+++ src/data/pacientes.js
@@ -0,0 +1,51 @@
+export const pacientes = [
+  {
+    id:1,
+    nombre:'Sebastian Mesa',
+    tipo_de_documento:'CC',
+    documento:16456,
+    descripcion:''
+  },
+  {
+    id:2,
+    nombre:'Carlos Andres',
+    tipo_de_documento:'CC',
+    documento:13245,
+    descripcion:''
+  },
+  {
+    id:3,
+    nombre:'Gabriel Goez',
+    tipo_de_documento:'CC',
+    documento:14236,
+    descripcion:''
+  },
+  {
+    id:4,
+    nombre:'Victor Manuelle',
+    tipo_de_documento:'CC',
+    documento:163546,
+    descripcion:''
+  },
+  {
+    id:5,
+    nombre:'Brigid Toro',
+    tipo_de_documento:'CC',
+    documento:16534,
+    descripcion:''
+  },
+  {
+    id:6,
+    nombre:'Madelei(n*99)e',
+    tipo_de_documento:'CC',
+    documento:16543,
+    descripcion:''
+  },
+  {
+    id:7,
+    nombre:'Richard',
+    tipo_de_documento:'CC',
+    documento:16543,
+    descripcion:''
+  }
+]
